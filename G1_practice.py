2ae1243e6375f4430e6c4f4f12c5224438c79649
julsy-chen <68758478+julsy-chen@users.noreply.github.com>
2024-07-30 14:02:00 -0400

    Add files via upload

---
+++ G1_practice.py
@@ -0,0 +1,18 @@
+# G1 Practice Questions
+
+question_dict_answer = {1 : {"question" : "In Ontario, a driver will be give 7 demerit points if he/she is covicted of :", "choices" : ["A. Following too close", "B. Failing to stop for a stopped school bus with its red alternating lights on", "C. Failing to remain at the scene of a accident", "D. Failing to obey the directions of a police officer"], "answer" : 'C'},
+                        2 : {"question" : "In Onatrio, a first conviction for drinking and driving can bring a minimum license suspension of:", "choices" : ["A. 6 months", "B. 12 months", "C. 24 months", "D. 48 months"], "answer" : 'B'},
+                        3 : {"question" : "In Ontario, it is legal to turn right on red:", "choices" : ["A. Only after the vehicle has come to a complte stop yielding right of way", "B. Only from a one-way street to another one-way street", "C. Only from a two-way street to another two-way street", "D. Only from a one-way street to a two-way street"], "answer" : 'A'},
+                        4 : {"question" : "A streetcar is stopped to load annd unload passengers. A driver of a motor vehicle is approaching this streetcar from the rear. The driver must stop:", "choices" : ["A. 2 meters from the rear-most door at which the passengers are getting on or off", "B. 3 meters from the rear-most door at which the passengers are getting on or off", "C. 6 meters from the rear-most door at which the passengers are getting on or off", "D. 9 meters from the rear-most door at which the passengers are getting on or off"], "answer" : 'A'},
+                        5 : {"question" : "When approaching a level railway crossing with the gates down, the driver of a motor vehicle must:", "choices" : ["A. Stop 1 meter from the gates", "B. Stop 3 meters from the line", "C. Stop 5 meters from the first rail", "D. Stop 15 meters from the railroad crossing"], "answer" : 'C'},
+                        6 : {"question" : "In what lane should you drive if you intend to make a ride turn?", "choices" : ["A. Close to the left of the roadway", "B. Close to the right hand side of the roadway", "C. Close to the centerline of the roadway", "D. Does not matter"], "answer" : 'B'}
+                        #7 : {"question" : "", "choices" : ["A. ", "B. ", "C. ", "D. "], "answer" : ''},
+                        #8 : {"question" : "", "choices" : ["A. ", "B. ", "C. ", "D. "], "answer" : ''},
+                        #9 : {"question" : "", "choices" : ["A. ", "B. ", "C. ", "D. "], "answer" : ''},
+                        #10 : {"question" : "", "choices" : ["A. ", "B. ", "C. ", "D. "], "answer" : ''},
+                        #11 : {"question" : "", "choices" : ["A. ", "B. ", "C. ", "D. "], "answer" : ''},
+                        #12 : {"question" : "", "choices" : ["A. ", "B. ", "C. ", "D. "], "answer" : ''},
+                        }
+
+for q_num in question_dict_answer:
+    print(q_num)
